9350ebb8e4e1e43644271c3d02d27254aaada10f
justinchin <justinchin.professionalcareer@gmail.com>
2025-11-19 18:27:01 -0500

    added image build support

--- langchain-llmops/server.py
+++ langchain-llmops/server.py
@@ -27,4 +27,4 @@ async def query_llm(prompt_data: PromptCreate):
 
 if __name__ == "__main__":
    import uvicorn
-   uvicorn.run(app, host="127.0.0.1", port=8000)
+   uvicorn.run(app, host="127.0.0.1", port=80)
